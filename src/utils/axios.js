6a854eef28f8d8383bed760199cdbcb780b076f5
ZZOMAN <reumia@gmail.com>
2018-01-11 17:03:32 +0900

    Axios : Add Cache on HTTP Request

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,12 @@
 import axios from 'axios'
+import { setupCache } from 'axios-cache-adapter'
+
+const cache = setupCache({
+  maxAge: 15 * 60 * 1000
+})
 
 export default axios.create({
   baseURL: process.env.API_ENDPOINT,
-  timeout: 10000
+  timeout: 10000,
+  adapter: cache.adapter
 })
